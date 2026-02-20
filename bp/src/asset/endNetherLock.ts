cdfa71eb9cfa73fa04a9c7eb46a41d6d646ebdcc
basicengine92-tech <basicengine92@gmail.com>
2026-02-20 16:52:41 +0800

    Update endNetherLock.ts

--- bp/src/asset/endNetherLock.ts
+++ bp/src/asset/endNetherLock.ts
@@ -1,41 +1,42 @@
 import { PlayerDimensionChangeAfterEvent, PlayerSpawnAfterEvent, world } from "@minecraft/server";
 import { get } from "../util/database";
+
+function _tryLockDimension(player) {
+    if (player.isOp()) return;
+    const dimensionId = player.dimension.id;
+    const lockEnd = get("endLock");
+    const lockNether = get("netherLock");
+    if ((lockNether && dimensionId === "minecraft:nether") || (lockEnd && dimensionId === "minecraft:the_end")) {
+        player.teleport(world.getDefaultSpawnLocation(), {
+            dimension: world.getDimension("minecraft:overworld"),
+        });
+    }
+}
+
 export function endNetherLockOn() {
     world.afterEvents.playerDimensionChange.subscribe(onDimensionChange);
     world.afterEvents.playerSpawn.subscribe(onJoin);
 }
+
 export function endNetherLockOff() {
     world.afterEvents.playerDimensionChange.unsubscribe(onDimensionChange);
     world.afterEvents.playerSpawn.unsubscribe(onJoin);
 }
+
 export function checkNetherEnd() {
     const lockEnd = get("endLock");
     const lockNether = get("netherLock");
-    const overworld = world.getDimension("minecraft:overworld");
+    if (!lockEnd && !lockNether) return;
     world.getAllPlayers().forEach((player) => {
-        if (player.isOp()) return;
-        const dimension = player.dimension.id;
-        if ((lockNether && dimension === "minecraft:nether") || (lockEnd && dimension === "minecraft:the_end")) {
-            player.teleport(world.getDefaultSpawnLocation(), {
-                dimension: overworld,
-            });
-        }
+        _tryLockDimension(player);
     });
 }
-function onDimensionChange({ toDimension: { id }, player }: PlayerDimensionChangeAfterEvent) {
-    if (player.isOp()) return;
-    if ((get("netherLock") && id === "minecraft:nether") || (get("endLock") && id === "minecraft:the_end")) {
-        player.teleport(world.getDefaultSpawnLocation(), {
-            dimension: world.getDimension("minecraft:overworld"),
-        });
-    }
+
+function onDimensionChange({ player }: PlayerDimensionChangeAfterEvent) {
+    _tryLockDimension(player);
 }
+
 function onJoin({ player, initialSpawn }: PlayerSpawnAfterEvent) {
-    if (!initialSpawn || player.isOp()) return;
-    const dimension = player.dimension.id;
-    if ((get("netherLock") && dimension === "minecraft:nether") || (get("endLock") && dimension === "minecraft:the_end")) {
-        player.teleport(world.getDefaultSpawnLocation(), {
-            dimension: world.getDimension("minecraft:overworld"),
-        });
-    }
+    if (!initialSpawn) return;
+    _tryLockDimension(player);
 }
